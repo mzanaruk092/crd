88887d333330e036d10dda76e6a31a1550899657
Theptat Keawkanda <Theptat-k@rmutp.ac.th>
2022-11-22 11:03:26 +0700

    crd

---
+++ src/components/Employees.js
@@ -0,0 +1,16 @@
+const Employees =[
+{
+    id:"1",
+    Name:"Aumnaj jalearn",
+    Age:"25"
+},{
+    id:"2",
+    Name:"Viroj Rojana",
+    Age:"35"
+},{
+    id:"3",
+    Name:"Fernny Fino",
+    Age:"22"
+}
+]
+export default Employees;
